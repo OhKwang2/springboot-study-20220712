50adea779d40acc4f0fe8545915076051c4edba7
OhKwang2 <dhrhkd007@naver.com>
2022-07-12 20:53:13 +0900

    2022.07.12 - init commit

---
+++ src/main/java/com/springboot/studykok/StudyKokApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.studykok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyKokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyKokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/studykok/StudyKokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studykok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyKokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
